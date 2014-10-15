69023b8d1f0c81947a637c50a03d66d349217670
Kevin Takaoka <kevin.takaoka@gmail.com>
2014-10-15 01:11:20 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def process_name
+    @name = params["name"]
+    if @name == "Kevin"
+      @message = "Hey, "
+    else
+      @message = "Hello, "
+    end
+  end
+  
+  def get_city
+  end
+  
+  def process_city
+    if params["city"] == "Chicago"
+      @message = "Awsome"
+    else
+      @message = "...bummer"
+    end
+  end
+end
